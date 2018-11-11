acd763b401fd7cebc2aff2785aacbd304ec6e5eb
JJ Merelo <jjmerelo@gmail.com>
2018-11-11 19:54:30 +0100

    Makes some corrections and adds plotting for (wrong) data
    Conclusions anyway is that for a generation gap of 32, it takes longer to find a solution, with an increase in the number of evaluations and thus time.

--- utils/chart.R
+++ utils/chart.R
@@ -2,6 +2,8 @@ library(reshape2)
 library(ggplot2)
 library(ggthemes)
 
-data <- read.csv('../data/8generations.csv')
+data.8 <- read.csv('../data/8generations.csv')
+data.32 <- read.csv('../data/32generations.csv')
 
-ggplot(data,aes(x=Threads,y=Evaluations,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Evaluations",title="Generation gap = 8")
+ggplot(data.8,aes(x=Threads,y=Evaluations,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Evaluations",title="Generation gap = 8")
+ggplot(data.32,aes(x=Threads,y=Evaluations,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Evaluations",title="Generation gap = 8")
