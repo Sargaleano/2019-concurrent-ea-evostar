58d21641c731fab7ea49617fd12d70d5a47d0989
JJ Merelo <jjmerelo@gmail.com>
2018-11-11 20:05:53 +0100

    Plotting generations against time
    This was somewhing we had not explored before, the influence of the generation gap on the overall quality of the solutions, and it happens that when the generation gap increases the number of evaluations needed to find the solution decreases.
    
    This is probably due to the fact that there is less comunication and more computation, and since mixing happens at the same time (another thread) than the evolutionary algorithm, this allows for a bit more paralellism. However, it takes on average more generations to find the solution, so this is not such a big deal...
    
    We'll have to find the right balance of generation gap so that communication is optimal while the number of generations is also optimal.

--- utils/chart.R
+++ utils/chart.R
@@ -7,3 +7,6 @@ data.32 <- read.csv('../data/32generations.csv')
 
 ggplot(data.8,aes(x=Threads,y=Evaluations,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Evaluations",title="Generation gap = 8")
 ggplot(data.32,aes(x=Threads,y=Evaluations,group=Threads))+geom_boxplot()+scale_x_log10()+theme_tufte()+labs(x="Threads",y="Evaluations",title="Generation gap = 8")
+
+data <- data.frame(generations=c(rep(8,length(data.8$Time)),rep(32,length(data.32$Time))),time=c(data.8$Time,data.32$Time),evaluations=c(data.8$Evaluations,data.32$Evaluations))
+ggplot(data,aes(x=evaluations,y=time,color=generations))+geom_point()+theme_tufte()+labs(x="Evaluations",y="Time",title="Evaluations vs Time per generation gap")
