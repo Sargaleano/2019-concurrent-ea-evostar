94035fc128755e3679a497481950b4acd6cb111e
JJ Merelo <jjmerelo@gmail.com>
2018-11-11 20:13:08 +0100

    Plotting the number of evaluations vs. generation gap
    The fact that a bigger generation gap, which is equivalent to a time to migration, implies more evaluations is probably due to the loss of diversity. This could be counter-balanced with more populations or with a bigger initial population. We will have to run some tests to check for that.

--- utils/chart.R
+++ utils/chart.R
@@ -10,3 +10,6 @@ ggplot(data.32,aes(x=Threads,y=Evaluations,group=Threads))+geom_boxplot()+scale_
 
 data <- data.frame(generations=c(rep(8,length(data.8$Time)),rep(32,length(data.32$Time))),time=c(data.8$Time,data.32$Time),evaluations=c(data.8$Evaluations,data.32$Evaluations))
 ggplot(data,aes(x=evaluations,y=time,color=generations))+geom_point()+theme_tufte()+labs(x="Evaluations",y="Time",title="Evaluations vs Time per generation gap")
+
+ggplot(data,aes(x=generations,y=evaluations,group=generations))+geom_boxplot()+theme_tufte()+labs(title="Evaluations vs generation gap")
+ggplot(data,aes(x=generations,y=time,group=generations))+geom_boxplot()+theme_tufte()+labs(title="Time vs generation gap")
