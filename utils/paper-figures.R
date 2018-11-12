0a8872d5317af1830b0da1ca2e0e64ee7c8ce1ad
JJ Merelo <jjmerelo@gmail.com>
2018-11-12 19:52:30 +0100

    Adds info on experiments

--- utils/paper-figures.R
+++ utils/paper-figures.R
@@ -3,7 +3,8 @@ library(ggthemes)
 
 data <- read.csv("data/evostar2019.csv")
 data$gap = as.factor(data$gap)
-ggplot(data,aes(x=evaluations,y=time,color=gap))+scale_color_brewer(palette="Set1")+geom_point(aes()+theme_tufte()+labs(x="Evaluations",y="Time",title="Evaluations vs Time per generation gap")
+
+ggplot(data,aes(x=evaluations,y=time,color=gap))+scale_color_brewer(palette="Set1")+geom_point(size=3,aes(shape=gap))+theme_tufte()+labs(x="Evaluations",y="Time",title="Evaluations vs Time per generation gap")
 
 
 ggplot(data,aes(x=gap,y=evaluations,group=gap))+geom_boxplot()+theme_tufte()+labs(title="Evaluations vs generation gap")
