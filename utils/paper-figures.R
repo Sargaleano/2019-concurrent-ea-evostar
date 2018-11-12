f3c3e8f7a672259229ac995212b6f2a852d2df44
JJ Merelo <jjmerelo@gmail.com>
2018-11-12 19:28:51 +0100

    Adds with only paper figures

---
+++ utils/paper-figures.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+library(ggthemes)
+
+data <- read.csv("data/evostar2019.csv")
+data$gap = as.factor(data$gap)
+ggplot(data,aes(x=evaluations,y=time,color=gap))+scale_color_brewer(palette="Set1")+geom_point()+theme_tufte()+labs(x="Evaluations",y="Time",title="Evaluations vs Time per generation gap")
+
+
+ggplot(data,aes(x=gap,y=evaluations,group=gap))+geom_boxplot()+theme_tufte()+labs(title="Evaluations vs generation gap")
+ggplot(data,aes(x=gap,y=time,group=gap))+geom_boxplot()+theme_tufte()+labs(title="Time vs generation gap")
+
+ggplot(data,aes(x=gap,y=avg.eval,group=gap))+geom_boxplot()+theme_tufte()+labs(title="Time vs generation gap")
